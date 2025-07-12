1f9e81a681a3312f73adf3d0eaebbeff63e78d58
Artem Liakhov <Liakhov.Artem@yandex.ru>
2025-07-12 19:52:50 +0300

    Add BoTorch to dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 
+
 with open("README.md", "r") as f:
     long_description = f.read()
+
 
 setup(
     name="waggon",
@@ -22,16 +24,17 @@ setup(
     python_requires=">=3.11",
     install_requires=[
         "GPy==1.13.2",
-        "gpytorch==1.10.0", 
+        "gpytorch>=1.10.0", 
         "matplotlib==3.7.5", 
         "numpy==1.26.0",
         "scikit_learn==1.3.2", 
         "scipy==1.12.0", 
         "setuptools==72.1.0",
         "torch==2.2.2", 
-        "torchbnn==1.2", 
+        "torchbnn==1.2",
         "torchensemble==0.1.9", 
         "tqdm==4.66.4", 
+        "botorch>=0.13.0"
         # "jax==0.5.0", 
         # "protes==0.3.11"
     ],
